582941c0c30b49fcee5ea6b67b565690a6d515f3
hackcoder1000 <aaravmehra0905@gmail.com>
2021-09-26 09:27:01 +0400

    Updated to 6.4.1   - Fixed something or the other

--- src/main/java/net/legiblesleet827/platinum/world/OreGeneration.java
+++ src/main/java/net/legiblesleet827/platinum/world/OreGeneration.java
@@ -1,11 +1,14 @@
 package net.legiblesleet827.platinum.world;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.legiblesleet827.platinum.PlatinumMod;
 import net.legiblesleet827.platinum.block.ModBlocks;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.YOffset;
 import net.minecraft.world.gen.decorator.RangeDecorator;
 import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
@@ -19,13 +22,13 @@ public class OreGeneration {
             .configure(new OreFeatureConfig(
                     OreFeatureConfig.Rules.BASE_STONE_OVERWORLD,
                     ModBlocks.PLATINUM_ORE.getDefaultState(),
-                    1))
+                    3))
             .decorate(RangeDecorator.RANGE.configure(new RangeDecoratorConfig(UniformHeightProvider.create(
                     YOffset.fixed(20),
                     YOffset.fixed(0)
             )))
             .spreadHorizontally()
-            .repeat(1));
+            .repeat(3));
 
     public static ConfiguredFeature<?, ?> DEEPSLATE_ORE_PLATINUM_OVERWORLD = Feature.ORE
             .configure(new OreFeatureConfig(
@@ -37,7 +40,7 @@ public class OreGeneration {
                     YOffset.fixed(0)
             )))
             .spreadHorizontally()
-            .repeat(1));
+            .repeat(3));
 
     public static void register() {
         RegistryKey<ConfiguredFeature<?, ?>> orePlatinumOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_KEY,
@@ -46,5 +49,7 @@ public class OreGeneration {
                 new Identifier(PlatinumMod.MOD_ID, "deepslate_ore_platinum_overworld"));
         Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, orePlatinumOverworld.getValue(), ORE_PLATINUM_OVERWORLD);
         Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, deepslateOrePlatinumOverworld.getValue(), DEEPSLATE_ORE_PLATINUM_OVERWORLD);
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, orePlatinumOverworld);
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, deepslateOrePlatinumOverworld);
     }
 }
